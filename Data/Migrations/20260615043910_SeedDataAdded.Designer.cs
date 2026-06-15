b83f9a27624ae85527aadda4082d4ee19d28aa2f
Shankar <2503691@cognizant.com>
2026-06-15 18:05:11 +0530

    seed roles

--- Data/Migrations/20260615043910_SeedDataAdded.Designer.cs
+++ Data/Migrations/20260615043910_SeedDataAdded.Designer.cs
@@ -4,6 +4,7 @@ using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260615043910_SeedDataAdded")]
+    partial class SeedDataAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -72,18 +75,6 @@ namespace Data.Migrations
                     b.HasKey("roleId");
 
                     b.ToTable("Roles");
-
-                    b.HasData(
-                        new
-                        {
-                            roleId = 1,
-                            roleName = "Admin"
-                        },
-                        new
-                        {
-                            roleId = 2,
-                            roleName = "Employee"
-                        });
                 });
 
             modelBuilder.Entity("ShiftMaster.models.User", b =>
@@ -144,21 +135,6 @@ namespace Data.Migrations
                     b.HasIndex("RoleID");
 
                     b.ToTable("Users");
-
-                    b.HasData(
-                        new
-                        {
-                            UserID = 1,
-                            DepartmentID = 1,
-                            Email = "admin@shiftmaster.com",
-                            EmployeeID = "EMP001",
-                            LocationID = 1,
-                            Name = "Admin User",
-                            PasswordHash = "AQAAAAEAACcQAAAAEExampleHashedPassword==",
-                            Phone = "9876543210",
-                            RoleID = 1,
-                            Status = "Active"
-                        });
                 });
 
             modelBuilder.Entity("shiftmaster.models.AttendanceRecord", b =>
