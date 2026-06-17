85899e34ac85bcf58d08ac0401847293f2d891de
Kumari <2503624@cognizant.com>
2026-06-17 10:27:23 +0530

    updated roaster controller

--- API/Controllers/RosterController.cs
+++ API/Controllers/RosterController.cs
@@ -9,7 +9,6 @@ using Services.DTOs;
 using Services.Interfaces;
 using Services.Mapper;
 using shiftmaster.models;
-
 namespace API.Controllers
 {
     [Route("api/[controller]")]
@@ -28,33 +27,19 @@ namespace API.Controllers
             this.db = db;
         }
 
-
         [HttpPost]
         public async Task<IActionResult> CreateRoster([FromBody] CreateRosterDto dto)
         {
             var res = mapper.Map<WeeklyRoster>(dto);
             await repository.AddAsync(res);
 
-            
-
-                LocationID = dto.LocationID,
-                DepartmentID = dto.DepartmentID,
-                WeekStartDate = dto.WeekStartDate.Date,
-                WeekEndDate = dto.WeekStartDate.Date.AddDays(6),
-                CreatedByID = dto.CreatedByID,
-                Status = RosterStatus.Draft
-            };
-
             return Ok(mapper.Map<RosterResponseDto>(res));
         }
 
-        //HLD Endpoint: GET /api/rosters/{locationId}/{week} (Action: Get roster)
-        //Note: '{week}' represents the week start date string (e.g., "2026-06-15")
-        [HttpGet]
+        //HLD Endpoint: GET /api/rosters/{locationId}/{week} (Action: Get roster)//Note: '{week}' represents the week start date string (e.g., "2026-06-15")        [HttpGet]
         [Route("{locationId:int}/{week}")]
         public async Task<IActionResult> GetRoster(int locationId, string week)
         {
-
 
             if (!DateTime.TryParse(week, out DateTime parsedDate))
             {
@@ -71,7 +56,6 @@ namespace API.Controllers
             return Ok(response);
 
         }
-
 
     }
 }
